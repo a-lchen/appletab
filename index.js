228abb8908fb540a1c7638679d9fc70a583d38a4
Rachel Zhang <Rachel@RachelMacBook.local>
2019-03-26 23:03:16 -0700

    try1

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const express = require('express')
 // makes an express app
 const app = express();
 
+let Parser = require('rss-parser');
+let parser = new Parser();
+
 // if we ever access url /, return something (return it in res.send)
 app.get('/rachel', (req, res) => {
   res.send('Loves Alex!')
@@ -11,8 +14,37 @@ app.get('/rachel', (req, res) => {
 
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  (async () => { // async just means run the following code asynchronously
+
+    // get the feed out
+    let feed = await parser.parseURL('https://news.google.com/rss/search?q=astronomy&hl=en-US&gl=US&ceid=US:en');
+
+    // print the feed title
+    console.log(feed.title);
+
+    // for item in feed : print (item.title and item.link)
+
+
+    // feed.items.forEach(item => {
+    //   console.log(item.title + ':' + item.link)
+    // });
+
+    // firstItem =
+
+    // const firstItem = something
+    // let firstItem = something
+
+    let firstItem = feed.items[0] // something
+    console.log(firstItem)
+    res.send(firstItem)
+    // res.send(firstItem.)
+
+  })();
+  // res.send('Hello World!')
 });
+
+
+
 
 // listen on the port 8000 of server
 app.listen(8000, () => {
